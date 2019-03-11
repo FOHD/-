673dcd1224ff3abfa34f591ad2c6b0264c07c77a
FOHD <47806183+FOHD@users.noreply.github.com>
2019-03-11 22:10:25 +0800

    Add files via upload

---
+++ js/page.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+
+/*var page={
+		"author":"周佳琪","date":"2019/2/21"
+	};
+	alert(page.author);
+	alert(page.date);
+	*/
+
+var page=
+	{
+	"author":[
+		"zjq","zzz",""
+	]
+}
